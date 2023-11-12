e79922274e284faf4a50e6a121582725899d9321
Alexandre Herran <97691510+TheHerran@users.noreply.github.com>
2023-11-11 22:07:36 -0300

    Fix: services code options

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,9 +3,10 @@ import axios from 'axios'
 const token = localStorage.getItem('@Easy:Token')
 
 export const BASE_URL = 'https://easy-cooking-api.onrender.com/api'
+export const BASE_URL_LOCAL = 'http://127.0.0.1:8000/api'
 export const BearerToken = `Bearer ${token}`
 
 export const Api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL_LOCAL,
   timeout: 15000,
 });
